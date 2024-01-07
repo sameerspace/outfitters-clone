90c7afc7e185a513d67e87a158a66871f89acc61
Salman4426 <salman.aziz@devsinc.com>
2024-01-08 01:59:26 +0500

    feat: little improvements in orm

---
+++ applications/outfitters-backend/src/config/orm.config.ts
@@ -0,0 +1,18 @@
+import { registerAs } from '@nestjs/config';
+import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+import { User } from 'src/entities/user.entity';
+
+export default registerAs(
+  'orm.config',
+  (): TypeOrmModuleOptions => ({
+    type: 'postgres',
+    host: process.env.DB_HOST,
+    port: parseInt(process.env.DB_PORT, 10),
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    entities: [User],
+    synchronize: true,
+    dropSchema: process.env.DB_DROP_SCHEMA === 'true',
+  }),
+);
