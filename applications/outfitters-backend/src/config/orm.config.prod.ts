99a32958ca835b5269e53c9ec0b8b59459c9631b
Sameer <sameer.sam2k17@gmail.com>
2024-01-10 15:57:58 +0500

    updated typeorm config

--- applications/outfitters-backend/src/config/orm.config.prod.ts
+++ applications/outfitters-backend/src/config/orm.config.prod.ts
@@ -1,18 +1,17 @@
 import { registerAs } from '@nestjs/config';
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
-import { User } from 'src/entities/user.entity';
+import { User } from 'src/users/cats.entity';
 
 export default registerAs(
   'orm.config',
   (): TypeOrmModuleOptions => ({
     type: 'postgres',
     host: process.env.DB_HOST,
-    port: parseInt(process.env.DB_PORT, 10),
+    port: parseInt(process.env.DB_PORT, 5432),
     username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
     entities: [User],
     synchronize: false,
-    dropSchema: process.env.DB_DROP_SCHEMA === 'true',
   }),
 );
---
+++ applications/outfitters-backend/src/users/cats.entity.ts
@@ -0,0 +1,13 @@
+import { BeforeInsert, Entity, PrimaryColumn } from 'typeorm';
+import ulid from 'ulid';
+
+@Entity()
+export class User {
+  @PrimaryColumn()
+  id: string;
+
+  @BeforeInsert()
+  generateUlid() {
+    this.id = ulid.ulid();
+  }
+}
