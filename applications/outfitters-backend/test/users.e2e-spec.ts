f80806c7a82234e9647e35df02f1580a4a4fb20f
Sameer <sameer.sam2k17@gmail.com>
2024-01-25 15:51:10 +0500

    added e2e tests for users

--- applications/outfitters-backend/test/users.e2e-spec.ts
+++ applications/outfitters-backend/test/users.e2e-spec.ts
@@ -2,9 +2,11 @@ import { INestApplication } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
 import request from 'supertest';
 import { AppModule } from '../src/app.module';
+import { CreateUserDto } from '../src/users/dto/create-user.dto';
 
 describe('UserController (e2e)', () => {
   let app: INestApplication;
+  let server: any;
 
   beforeEach(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
@@ -13,13 +15,27 @@ describe('UserController (e2e)', () => {
 
     app = moduleFixture.createNestApplication();
     await app.init();
+    server = app.getHttpServer();
   });
 
   it('/users (GET) 200 OK', async () => {
-    const server = app.getHttpServer();
-
     const response = await request(server).get('/users');
     expect(response.status).toBe(200);
     expect(Array.isArray(response.body)).toBe(true);
+  });
+
+  it('/users (POST) 201 CREATED', async () => {
+    const mockUser: CreateUserDto = {
+      email: 'test@test.com',
+      firstName: 'sam',
+      lastName: 'test',
+      password: 'Testing123',
+    };
+    const response = await request(server).post('/users').send(mockUser);
+    expect(response.status).toBe(201);
+  });
+
+  afterAll(async () => {
+    await app.close();
   });
 });
