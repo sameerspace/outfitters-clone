4b8a71cda691a040ff6fb95b664db8e9f720f0f7
Sameer <sameer.sam2k17@gmail.com>
2024-01-31 16:57:38 +0500

    fixup

--- applications/outfitters-backend/test/users.e2e-spec.ts
+++ applications/outfitters-backend/test/users.e2e-spec.ts
@@ -2,7 +2,6 @@ import { INestApplication } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
 import request from 'supertest';
 import { AppModule } from '../src/app.module';
-import { CreateUserDto } from '../src/users/dto/create-user.dto';
 
 describe('UserController (e2e)', () => {
   let app: INestApplication;
@@ -18,22 +17,10 @@ describe('UserController (e2e)', () => {
     server = app.getHttpServer();
   });
 
-  it('/users (GET) 200 OK', async () => {
+  it('/api/v1/users (GET) 200 OK', async () => {
     const response = await request(server).get('/users');
     expect(response.status).toBe(200);
     expect(Array.isArray(response.body)).toBe(true);
-  });
-
-  it('/users (POST) 201 CREATED', async () => {
-    const mockUser: CreateUserDto = {
-      email: 'test@test.com',
-      username: 'samistesting',
-      firstName: 'sam',
-      lastName: 'test',
-      password: 'Testing123',
-    };
-    const response = await request(server).post('/users').send(mockUser);
-    expect(response.status).toBe(201);
   });
 
   afterAll(async () => {
