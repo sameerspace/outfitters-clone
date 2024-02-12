6387cb0251f0b4cf4f81d9434d57ff735483420d
Sameer <sameer.sam2k17@gmail.com>
2024-02-12 16:36:26 +0500

    small improvs

--- applications/outfitters-frontend/src/app/account/register/page.tsx
+++ applications/outfitters-frontend/src/app/account/register/page.tsx
@@ -2,21 +2,25 @@ const Page = () => {
   return (
     <div className="flex justify-center h-[38rem] ">
       <div className="flex flex-col w-[30rem]  max-w-[47.8rem] pt-20 items-center">
-        <h1 className="font-bold text-[2.5rem] mb-14">Login</h1>
+        <h1 className="font-bold text-[2.5rem] mb-14">Create Account</h1>
+        <input
+          className="px-4 py-[12px] w-full mb-4 border border-black"
+          placeholder="First Name"
+        />
+        <input
+          className="px-4 py-[12px] w-full mb-4 border border-black"
+          placeholder="Last Name"
+        />
         <input
           className="px-4 py-[12px] w-full mb-4 border border-black"
           placeholder="Email"
         />
         <input
           className="px-4 py-[12px] w-full mb-4 border border-black"
-          placeholder="Password"
+          placeholder="password"
         />
-        <div className="flex items-start w-full">
-          <p className="text-base">FORGOT YOUR PASSWORD?</p>
-        </div>
         <div className="flex py-8 flex-col items-center">
-          <button className="bg-black text-white p-2 w-full">Sign in</button>
-          <h3 className="text-xl font-light pt-4 w-full">Create Account</h3>
+          <button className="bg-black text-white px-6 py-2 w-full">Create</button>
         </div>
       </div>
     </div>
--- applications/outfitters-frontend/src/components/Footer/EmailFooter/EmailFooter.tsx
+++ applications/outfitters-frontend/src/components/Footer/EmailFooter/EmailFooter.tsx
@@ -6,17 +6,17 @@ const EmailFooter: React.FC = () => {
     <div className="flex flex-col w-full h-52 items-center justify-evenly pb-10">
       <div className="text-2xl">GET THE LATEST TRENDS FIRST</div>
       <input
-        className="w-1/3  h-10 border-b-2 border-black placeholder:text-center placeholder:text-black placeholder:font-thin focus:outline-none placeholder:focus:text-xs focus:placeholder:mb-10"
+        className="w-[35rem] h-10 border-b-[1px] border-black placeholder:text-center placeholder:text-black placeholder:font-thin focus:outline-none placeholder:focus:text-xs focus:placeholder:mb-10"
         type="text"
         placeholder="ENTER YOUR EMAIL"
       />
-      <div className="flex justify-evenly w-1/4">
-        <ImFacebook2 size={25} />
-        <FaInstagram size={25} />
-        <FaTiktok size={25} />
-        <FaYoutube size={25} />
-        <FaTwitter size={25} />
-        <FaPinterest size={25} />
+      <div className="flex justify-center gap-10 w-1/4 mt-4">
+        <ImFacebook2 size={22} />
+        <FaInstagram size={22} />
+        <FaTiktok size={22} />
+        <FaYoutube size={22} />
+        <FaTwitter size={22} />
+        <FaPinterest size={22} />
       </div>
     </div>
   );
