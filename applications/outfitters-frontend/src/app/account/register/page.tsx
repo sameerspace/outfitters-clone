347c66c7e78a134d1024cfdc5f104eb184934bc8
Sameer <sameer.sam2k17@gmail.com>
2024-02-13 19:32:04 +0500

    added formik and submit call

--- applications/outfitters-frontend/src/app/account/register/page.tsx
+++ applications/outfitters-frontend/src/app/account/register/page.tsx
@@ -1,29 +1,79 @@
+'use client';
+
+import apiClient from '@/configs/axios.config';
+import { RegisterSchema } from '@/validations/register.schema';
+import { ErrorMessage, Field, Form, Formik } from 'formik';
+import { useRouter } from 'next/navigation';
+import { FormEvent } from 'react';
+
 const Page = () => {
+  const router = useRouter();
+
+  const submitFOrm = async (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    const formData = new FormData(event.currentTarget);
+    try {
+      const response = await apiClient.post('/auth/register', formData, {
+        headers: { 'Content-Type': 'application/json' },
+      });
+      router.replace('/');
+    } catch (error: any) {
+      console.log(error.response.data.message);
+    }
+  };
+
   return (
-    <div className="flex justify-center h-[38rem] ">
-      <div className="flex flex-col w-[19rem] md:w-[28rem] max-w-[47.8rem] pt-20 items-center">
-        <h1 className="font-bold text-[2.5rem] mb-14">Create Account</h1>
-        <input
-          className="px-4 py-[12px] w-full mb-4 border border-black"
-          placeholder="First Name"
-        />
-        <input
-          className="px-4 py-[12px] w-full mb-4 border border-black"
-          placeholder="Last Name"
-        />
-        <input
-          className="px-4 py-[12px] w-full mb-4 border border-black"
-          placeholder="Email"
-        />
-        <input
-          className="px-4 py-[12px] w-full mb-4 border border-black"
-          placeholder="password"
-        />
-        <div className="flex py-8 flex-col items-center">
-          <button className="bg-black text-white px-6 py-2 w-full">Create</button>
-        </div>
-      </div>
-    </div>
+    <Formik
+      initialValues={{}}
+      onSubmit={() => {}}
+      validationSchema={RegisterSchema}
+    >
+      {({ isSubmitting }) => (
+        <Form>
+          <div className="flex justify-center h-[38rem] ">
+            <div className="flex flex-col w-[19rem] md:w-[28rem] max-w-[47.8rem] pt-20 items-center">
+              <h1 className="font-bold text-[2.5rem] mb-14">Create Account</h1>
+              <Field
+                className="px-4 py-[12px] w-full mb-4 border border-black"
+                name="firstName"
+                placeholder="First Name"
+              />
+              <ErrorMessage
+                name="firstName"
+                component="div"
+                className="text-red-600"
+              />
+              <Field
+                className="px-4 py-[12px] w-full mb-4 border border-black"
+                placeholder="Last Name"
+                name="lastName"
+              />
+              <Field
+                className="px-4 py-[12px] w-full mb-4 border border-black"
+                placeholder="Email"
+                name="email"
+              />
+              <Field
+                className="px-4 py-[12px] w-full mb-4 border border-black"
+                placeholder="password"
+                name="password"
+                type="password"
+              />
+              <div className="flex py-8 flex-col items-center">
+                <button
+                  type="submit"
+                  className="bg-black text-white px-6 py-2 w-full"
+                  disabled={isSubmitting}
+                >
+                  Create
+                </button>
+              </div>
+            </div>
+          </div>
+        </Form>
+      )}
+    </Formik>
   );
 };
 
---
+++ applications/outfitters-frontend/src/validations/register.schema.ts
@@ -0,0 +1,10 @@
+import * as Yup from 'yup';
+
+export const RegisterSchema = Yup.object().shape({
+  firstName: Yup.string()
+    .min(2, 'First name should be more than 2 characters')
+    .required('Please enter your first name'),
+  lastName: Yup.string().min(2).required(),
+  email: Yup.string().email().required(),
+  password: Yup.string().min(8).required(),
+});
